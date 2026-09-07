db27cfe4dc493fec39c8ee5b2386d22c4780116b
Youssef Mohamed <127245480+yousef0mohamed@users.noreply.github.com>
2026-09-07 16:33:33 +0300

    Update date columns in crm_sales_details table
    Changed data types for order dates in crm_sales_details table.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -52,15 +52,15 @@ GO
 
 CREATE TABLE silver.crm_sales_details
 (
-	sls_ord_num		  NVARCHAR(50),
-	sls_prd_key		  NVARCHAR(50),
-	sls_cust_id		  INT,
-	sls_order_dt	  INT,
-	sls_ship_dt		  INT,
-	sls_due_dt	  	INT,
-	sls_sales		    INT,
-	sls_quantity	  INT,
-	sls_price	    	INT,
+	sls_ord_num		NVARCHAR(50),
+	sls_prd_key		NVARCHAR(50),
+	sls_cust_id		INT,
+	sls_order_dt	DATE,
+	sls_ship_dt		DATE,
+	sls_due_dt	  	DATE,
+	sls_sales		INT,
+	sls_quantity	INT,
+	sls_price	    INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
