e8e91f104d7d34b9f314c787f826fda1bd4d54a4
Youssef Mohamed <127245480+yousef0mohamed@users.noreply.github.com>
2026-09-07 06:26:35 +0300

    Create silver schema tables for CRM and ERP data
    This script creates various tables in the 'silver' schema, including customer, product, sales details, and ERP tables, while dropping existing tables if they exist.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,1 +1,110 @@
+/*
+===============================================================================
+DDL: Create Silver Tables
+===============================================================================
+Purpose:
+    This creates tables in the 'silver' schema, dropping existing tables , if they already exist.
+	Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
+USE DWH_Prj_1
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
+GO
+
+CREATE TABLE silver.crm_cust_info
+(
+	cst_id				      INT,
+	cst_key				      NVARCHAR(50),
+	cst_firstname		    NVARCHAR(50),
+	cst_lastname	    	NVARCHAR(50),
+	cst_marital_status	NVARCHAR(50),
+	cst_gndr			      NVARCHAR(50),
+	cst_create_date		  DATE,
+	dwh_create_date		  DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- prd_info Table
+IF OBJECT_ID('silver.crm_prd_info' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
+GO
+
+CREATE TABLE silver.crm_prd_info
+(
+	prd_id			    INT,
+	prd_key			    NVARCHAR(50),
+	prd_nm			    NVARCHAR(50),
+	prd_cost		    INT,
+	prd_line	  	  NVARCHAR(50),
+	prd_start_dt	  DATETIME,
+	prd_end_dt		  DATETIME,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- sales_details Table
+IF OBJECT_ID('silver.crm_sales_details' , 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+GO
+
+CREATE TABLE silver.crm_sales_details
+(
+	sls_ord_num		  NVARCHAR(50),
+	sls_prd_key		  NVARCHAR(50),
+	sls_cust_id		  INT,
+	sls_order_dt	  INT,
+	sls_ship_dt		  INT,
+	sls_due_dt	  	INT,
+	sls_sales		    INT,
+	sls_quantity	  INT,
+	sls_price	    	INT,
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+---------------------------------------------------------------------
+----------------------------- ERP Tables ----------------------------
+
+-- cust_az12 Table
+IF OBJECT_ID('silver.erp_cust_az12' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
+GO
+
+CREATE TABLE silver.erp_cust_az12
+(
+	cid				      NVARCHAR(50),
+	bdate			      DATE,
+	gen			      	NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- loc_a101 Table
+IF OBJECT_ID('silver.erp_loc_a101' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101;
+GO
+
+CREATE TABLE silver.erp_loc_a101
+(
+	cid			      	NVARCHAR(50),
+	cntry		      	NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
+
+-- px_cat_g1v2 Table
+IF OBJECT_ID('silver.erp_px_cat_g1v2' , 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE silver.erp_px_cat_g1v2
+(
+	id		      		NVARCHAR(50),
+	cat			      	NVARCHAR(50),
+	subcat		     	NVARCHAR(50),
+	maintenance	  	NVARCHAR(50),
+	dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+GO
